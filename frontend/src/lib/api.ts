36d264833319e414f3f89c33540f7f7b00ae3998
Somesh Gundayya <163583278+SomeshGundayya@users.noreply.github.com>
2026-04-19 13:56:06 +0530

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hosteller-rc6z.onrender.com",
+  baseURL: "https://hosteller-rc6z.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
